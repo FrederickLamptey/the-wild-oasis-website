b7c608597dc7fa4222508e3e744129a5d91ddb9d
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-01-08 18:49:21 -0500

    optimize logo image

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,6 @@ const josefin = Josefin_Sans({
   display: 'swap',
 });
 
-
 export const metadata = {
   // title: 'The Wild Oasis',
   title: {
@@ -31,8 +30,8 @@ export default function RootLayout({ children }) {
       >
         <Header />
 
-        <div className='flex-1 px-8 py-12'>
-          <main className='max-w-7xl mx-auto'>{children}</main>
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
         </div>
       </body>
     </html>
