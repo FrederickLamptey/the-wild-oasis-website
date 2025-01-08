698047f43a3d4561f36967d8d228ab5f3a5d6be8
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-01-07 20:22:04 -0500

    organize project

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import Navigation from "./components/Navigation";
+import Link from 'next/link';
+import Navigation from './_components/Navigation';
 
 export default function Page() {
   return (
