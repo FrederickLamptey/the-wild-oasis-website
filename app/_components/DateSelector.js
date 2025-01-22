a0e8e8caf69dfd765eaff73cd3bf90ef19aed118
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-01-22 17:55:36 -0500

    use the contextApi to provide the range state to app tree

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -3,6 +3,7 @@
 import { isWithinInterval } from 'date-fns';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import { useReservation } from './ReservationContext';
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -15,7 +16,7 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, cabin, bookedDates }) {
-
+  const { range, setRange } = useReservation();
 
   // CHANGE
   const regularPrice = 23;
--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import '@/app/_styles/globals.css';
 
 import { Josefin_Sans } from 'next/font/google';
 import Header from './_components/Header';
+import { ReservationProvider } from './_components/ReservationContext';
 
 // configuring font
 const josefin = Josefin_Sans({
@@ -31,7 +32,9 @@ export default function RootLayout({ children }) {
         <Header />
 
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
